66668c59128591a7debf6428440c23dcdb3894ee
Andrew Tran <andrewtran559@gmail.com>
2024-01-27 01:14:13 -0800

    Update authenticateMiddleware.js

--- src/middleware/authenticateMiddleware.js
+++ src/middleware/authenticateMiddleware.js
@@ -1,13 +1,12 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res, next) => {
-  const token = req.cookies?.token;
+  const token = req.cookies.token;
 
   try {
     req.user = jwt.verify(token, process.env.MY_SECRET);
     next();
   } catch (error) {
-    console.error(error);
     res.sendStatus(401);
   }
 }
